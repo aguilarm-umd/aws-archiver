e4037086e705888dc513c0277d727d4b099f60b6
Marc Andreu Grillo Aguilar <aguilarm@umd.edu>
2023-04-19 11:48:22 -0400

    LIBITD-2311: Adding isort to dev requirement
    https://umd-dit.atlassian.net/browse/LIBITD-2311

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     install_requires=[i.strip() for i in open("requirements.txt").readlines()],
     python_requires='>=3.7',
     extras_require={  # Optional
-       'dev': ['pycodestyle'],
+       'dev': ['pycodestyle', 'isort'],
        'test': [],
     }
 )
